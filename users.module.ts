52e38a6f42301935845ffa5934a3036f453987f1
mreazy-79 <ultimatepluggs@gmail.com>
2026-06-18 11:20:30 +0200

    Updated user module

---
+++ users.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersService } from './services/users.service';
+import { UsersController } from './controllers/users.controller';
+import { User } from './entities/user.entity';
+import { Role } from '../roles/entities/role.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User, Role])],
+  providers: [UsersService],
+  controllers: [UsersController],
+  exports: [UsersService, TypeOrmModule],
+})
+export class UsersModule {}
